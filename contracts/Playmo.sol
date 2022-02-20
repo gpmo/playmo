6178aea97b397ac92305486a91e7958418b72b6d
Gaston Montemayor Olaizola <gastonmo@spotify.com>
2022-02-19 22:22:24 -0700

    Continue logic for playmo contract

--- contracts/Playmo.sol
+++ contracts/Playmo.sol
@@ -4,38 +4,61 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Playmo is Ownable{
+contract Playmo is Ownable {
     uint256 public playCost;
     uint256 public maxTopPlayers;
     uint256 public maxPlays;
-    mapping (address => Player) players;
-    mapping (uint256 => Player) topPlayers;
+    mapping(address => Player) players;
+    mapping(uint256 => Player) topPlayers;
     uint256 internal topPlayersCount;
 
+    // share weights
+    mapping(uint256 => uint256) sharesPerRanking;
+    uint256 ownerShares;
+
     struct Player {
+        address playerAddress;
         uint256 numTrials;
-        uint256 shares;
-        uint256 highScore;
+        uint256 score;
+        bool exists;
     }
 
-    function addTopPlayer(address playerAddress) internal view {
+    function addTopPlayer(address playerAddress) internal view {}
 
-    }
+    function updateScore(address playerAddress, uint256 newScore)
+        public
+        onlyOwner
+    {
+        // if newScore is smaller than lowest top score, halt early
+        if (newScore < topPlayers[topPlayersCount].score) {
+            return;
+        }
 
-    function updateScore(address playerAddress, uint256 score, uint256 numTrials)
-        public onlyOwner {
-            uint256 currRanking = -1;
-            for (uint256 i = topPlayersCount - 1; i >= 0 ; i--) {
-                if (score < topPlayers[i].highScore) {
-                    break;
-                }else if (score > topPlayers[i].highScore){
-                    currRanking = i;
+        // TODO: check if this already exists
+        // Get / Create player with new score
+        Player memory playerWithNewScore;
+        if (players[playerAddress].exists) {
+            playerWithNewScore = players[playerAddress];
+        } else {
+            playerWithNewScore = Player(playerAddress, 0, newScore, true);
+            players[playerAddress] = playerWithNewScore;
+        }
+
+        // update top players array if necessary
+        Player memory unswappedPlayer;
+        for (uint256 i = 0; i < topPlayersCount; i++) {
+            // if playerScore is larger than score for player i
+            if (newScore > topPlayers[i].score) {
+                // if newScore does not belong to the player i
+                if (topPlayers[i].playerAddress != playerAddress) {
+                    unswappedPlayer = topPlayers[i];
+                    Player memory player;
+                    topPlayers[i] = player;
+                    // if newScore belongs to the player i
+                } else {
+                    topPlayers[i].score = newScore;
                 }
-            }
-
-            if (currRanking < 0) { return; }
-
-            uint256 shares = (maxTopPlayers*10) - 10 * currRanking;
-            topPlayers[currRanking] = Player(numTrials, shares, score);
+            } else {}
+        }
     }
 }
